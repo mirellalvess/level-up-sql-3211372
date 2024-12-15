7412191433feabce6953b99d1df4598249acda11
Mirella Alves <110692094+mirellalvess@users.noreply.github.com>
2024-12-15 18:11:30 +0000

    Desafio 1 e 2 feito!

--- 01_restaurant/challenges/01_02_anniversary_ch.sql
+++ 01_restaurant/challenges/01_02_anniversary_ch.sql
@@ -5,3 +5,12 @@
 -- they plan to bring in their party.
 
 -- Hint: SQLite offers the INTEGER and REAL datatypes
+
+CREATE TABLE Invitation
+(CustomerID INTEGER PRIMARY KEY, 
+Guests INTEGER
+)
+
+INSERT INTO Invitation (CustomerID, Guests) VALUES (1, 3);
+
+SELECT * FROM Invitation
